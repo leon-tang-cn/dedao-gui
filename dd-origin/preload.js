e74816ef616373bcdc3d54a535493b63924ffafb
leon <letang@deloitte.com.cn>
2025-05-02 19:35:55 +0800

    chore: remove dd-origin directory
    - Deleted custom.js, main.js, package.json, and preload.js files
    - Removed entire dd-origin directory and its contents

--- dd-origin/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    sendExecuteMessage: (queryParams) => {
-      console.log('sendExecuteMessage', queryParams)
-      ipcRenderer.send('execute-method', queryParams)
-    }
-});
-    
